e63564dfb71c6258503f8b6f6c0ad8b6e881646b
Katsu0611 <katsuhiro.kotaki@keio.jp>
2025-02-25 19:20:16 +0900

    Added Lidar Code

---
+++ launch/rplidar.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            name='rplidar_node',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/ttyUSB1',
+                'serial_baudrate': 115200, 
+                'frame_id': 'laser_frame',
+                'scan_mode': 'Standard',
+                'inverted': False,
+                'angle_compensate': True,
+            }],
+        ),
+    ])
